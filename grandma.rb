10d874f8378b6b94f0c3825f0054da257b09e8ff
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-25 14:04:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(string)
   if string == string.upcase
     "NO, NOT SINCE 1938!"
     
-  elsif phrase == "I LOVE YOU GRANDMA!"  
+  elsif string == "I LOVE YOU GRANDMA!"  
     "I LOVE YOU TOO PUMPKIN!"
     
   else  
