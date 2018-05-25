91bc4e4820a394636665a382350d99450fc512a9
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-25 13:49:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase.upcase
+    
+    
+    "HUH?! SPEAK UP, SONNY!"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
